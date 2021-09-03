c712c5de75b04289a99b28315490e5ee57d04b14
katiacreator <katia.creator@gmail.com>
2021-09-03 15:49:49 -0400

    stubbed up all directories and home, base, and about html pages, also stubbed up views and urls.py files, created superuser

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+# from .models import 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+# def index(request):
+#     return HttpResponse("Hello, world. You're working correctly so far!")
+
+def home(request):
+  return render(request, 'home.html')
+def about(request):
+  return render(request, 'about.html')
