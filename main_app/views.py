2efeba2cd301b3fefe2bbc924dd4045a7f031fbb
katiacreator <katia.creator@gmail.com>
2021-09-03 18:35:44 -0400

    moved files into the right directory to resolve templatedoesnotexist bug

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,17 @@
-from django.shortcuts import redirect, render
-from .models import Bread, Task, Photo
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView, DetailView
-from django.contrib.auth.views import LoginView
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.shortcuts import redirect, render
+# from .models import Bread, Task, Photo
+# from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from django.views.generic import ListView, DetailView
+# from django.contrib.auth.views import LoginView
+# from django.contrib.auth import login
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
-# def index(request):
-#     return HttpResponse("Hello, world. You're working correctly so far!")
+from django.shortcuts import render
 
 def home(request):
   return render(request, 'home.html')
+
 def about(request):
   return render(request, 'about.html')
