58509ec534409ed783f20938ddf70c685f080134
katiacreator <katia.creator@gmail.com>
2021-09-04 15:23:55 -0400

    registered models with admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Bread, Task, Photo
 # Register your models here.
+admin.site.register(Bread)
+admin.site.register(Task)
+admin.site.register(Photo)
