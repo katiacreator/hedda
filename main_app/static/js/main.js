58da755fed44bccc8d4baa06f054f96b614da479
katiacreator <katia.creator@gmail.com>
2021-09-09 23:41:13 -0400

    about page complete

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -16,11 +16,11 @@ closeMenu.addEventListener('click', ()=>{
 
 /*===== GSAP ANIMATION =====*/
 // NAV
-gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:2, y: 10})
-gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.1, y: 30, stagger: 0.2,})
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:.8, y: 10})
+gsap.from('.nav__item', {opacity: 0, duration: 1, delay: .6, y: 30, stagger: 0.2,})
 
 // HOME
-gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
-gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
-gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+gsap.from('.home__title', {opacity: 0, duration: 1, delay:.6, y: 30})
+gsap.from('.home__description', {opacity: 0, duration: .7, delay:.4, y: 30})
+gsap.from('.home__button', {opacity: 0, duration: 1, delay:.7, y: 30})
+gsap.from('.home__img', {opacity: 0, duration: 1, delay:.6, y: 30})
